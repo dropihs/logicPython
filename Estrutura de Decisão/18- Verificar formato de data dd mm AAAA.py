c0ee0acae2a6e25ca43117315ab0abe7520b38fb
Pedro Soares <pedrosoaresty@gmail.com>
2021-05-01 17:51:12 -0300

    Exercicios 18 e 19
    Exercicio 19 fiz uma gambiarra imoral

--- Estrutura de Decisão/18- Verificar formato de data dd mm AAAA.py
+++ Estrutura de Decisão/18- Verificar formato de data dd mm AAAA.py
@@ -1,1 +1,28 @@
 #18.	Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+import sys #para usar a funcão de dar stop 
+
+
+dia = int(input("Digite o dia: "))
+if (dia < 0) or (dia >= 32):
+    print("Dia invalido\nEncerrando o sistema")
+    sys.exit()
+
+elif dia >= 1 and dia < 32:
+    print("Dia Valido")
+
+mes = int(input("Digite o mês: "))
+if (mes < 0) or (mes >= 12):
+    print("mês invalido\nEncerrando o sistema ")
+    sys.exit()
+
+elif mes > 1 and mes < 12:
+    print("mês Valido")
+
+ano = int(input("Digite o ano: "))
+if ano > 0: 
+    print("Ano valido")
+else: 
+    print("Ano invalido\nEncerrando o sistema")
+    sys.exit()
+
+print("data: {}/{}/{}".format(dia,mes,ano))
--- Estrutura de Decisão/19- Imprimir dezenas de um numero.py
+++ Estrutura de Decisão/19- Imprimir dezenas de um numero.py
@@ -1,4 +1,23 @@
-#19.	Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo.
+#19.Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo.
 #Observando os termos no plural a colocação do "e", da vírgula entre outros. Exemplo:
 #326 = 3 centenas, 2 dezenas e 6 unidades
 #12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
+num = int(input("Digite um numero 1-1000:   "))
+
+if num < 1000 and num > 0:
+    if 100 <= num <= 999:
+        u = num % 10
+        d = num % 100
+        c = num % 1000
+        print("Centena: ", str(c)[0], "\nDezena: ", str(d)[0], "\nUnidade: ", str(u)[0]) #dezena nao printa 0 WHYYYYYYYYYYYY
+    elif 10 < num <= 99: 
+        u = num % 10
+        d = num % 100
+        print("\nDezena: ", str(d)[0], "\nUnidade: ", str(u)[0])
+    elif 1 <= num <= 9: 
+        u = num % 10
+        print("Unidade: {}\n".format(u))
+else: 
+    print("Digito errado")
+
