ee0314492472a5d71fd72fed383fb69105c1ea22
Pedro Soares <pedrosoaresty@gmail.com>
2021-05-04 23:02:05 -0300

    Solução 19/19
    Adicinar o resto dos exercicios futuramente

--- 3) Estrutura de Repetição/18.py
+++ 3) Estrutura de Repetição/18.py
@@ -1,1 +1,15 @@
 #Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+#resolvido com lista 
+
+
+lista = []
+contador = 0
+
+quant = int(input("Digite a quantiade de número que deseja digitar: "))
+
+while quant != contador:
+    numero = lista.append(float(input("Digite um número: ")))
+    contador += 1
+
+print("Lista: ", lista, "\nMaior: ", max(lista), "\nMenor: ", min(lista))
+print("Soma: ", max(lista) + min(lista))
--- 3) Estrutura de Repetição/19.py
+++ 3) Estrutura de Repetição/19.py
@@ -1,1 +1,17 @@
 #Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
+
+lista = []
+contador = 0
+
+quant = int(input("Digite a quantiade de número que deseja digitar: "))
+while quant != contador:
+    numero = float(input("Digite um número: "))
+
+    while numero > 1000 or numero < 0:
+        numero = float(input("Digite um número[erro]: "))
+        
+    lista.append(numero)
+    contador += 1
+
+print("Lista: ", lista, "\nMaior: ", max(lista), "\nMenor: ", min(lista))
+print("Soma: ", max(lista) + min(lista))
