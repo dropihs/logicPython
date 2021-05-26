2f47664d226a668df53ee00a2dae9e59553bf6c8
Pedro Soares <pedrosoaresty@gmail.com>
2021-05-26 17:00:29 -0300

    Update ex13 (feito com dictionary)

--- 4) Listas/13.py
+++ 4) Listas/13.py
@@ -2,13 +2,25 @@
 # a média anual das temperaturas e mostre todas as temperaturas acima da 
 # média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 import random
-meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Aug', 'Set', 'Out','Nov', 'Dez']
+month = {	
+        0:'Janauary',
+		1:'February',
+		2:'March',
+		3:'April',
+		4:'May',
+		5:'June',
+		6:'July',
+		7:'August',
+		8:'September',
+		9:'October',
+		10:'November',
+		11:'December'	}
 tempMes = []
-m = [meses,tempMes]
 for i in range(12):
     tempMes.append(random.randint(1,30))
 
 mediaAnual = sum(tempMes)/12
 print("A media anual foi: ", mediaAnual)
+
 for i in range(12):
-    print("{} - {} Temp: {}".format(i,meses[i],tempMes[i]))
+    print("{} - {} Temp: {}".format(i,month.get(i),tempMes[i]))
